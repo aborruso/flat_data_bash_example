fc031eb9ee9a6958cb0cd114a894479b375f6a0f
Andrea Borruso <aborruso@gmail.com>
2021-05-20 17:36:24 +0200

    update

--- postprocessing.sh
+++ postprocessing.sh
@@ -6,7 +6,7 @@ set -u
 set -o pipefail
 
 # Install Miller
-sudo apt-get install Miller -y
+sudo apt-get install miller -y
 
 # retain only two columns of the input file
 mlr --csv -I cut -f data,ricoverati_con_sintomi $1
